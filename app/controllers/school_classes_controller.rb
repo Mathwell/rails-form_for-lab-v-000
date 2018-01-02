46b08b1abace6af6bbc7acaa5ea6777dc2a3b442
Olena Ageyeva <eageeva@hotmail.com>
2018-01-02 23:54:35 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -20,7 +20,7 @@ class SchoolClassesController<ApplicationController
 
   def update
     @class = SchoolClass.find(params[:id])
-    @class.update(post_params(:title, :room_number))
+    @class.update(class_params(:title, :room_number))
    redirect_to school_class_path(@post)
   end
 
